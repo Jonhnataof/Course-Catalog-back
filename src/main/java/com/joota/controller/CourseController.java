baf79c41329e74468da7d82fa1a0e0a64cacd71a
jonhnata <jonhnataof@gmail.com>
2025-06-06 19:28:52 -0300

    Criação do endpoint para criar os cursos

--- src/main/java/com/joota/controller/CourseController.java
+++ src/main/java/com/joota/controller/CourseController.java
@@ -4,10 +4,9 @@ import com.joota.model.Course;
 import com.joota.repository.CourseRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +21,12 @@ public class CourseController {
     public @ResponseBody List<Course> list() {
 
         return courseRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        return courseRepository.save(course);
+
     }
 }
